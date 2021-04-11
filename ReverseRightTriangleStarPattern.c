91f0ae586c0d8317f4cd8c9cd76115a5c66171da
Apoorva Srivastava <55443541+apoorvasrivastava98@users.noreply.github.com>
2021-04-11 23:44:20 +0530

    Create ReverseRightTriangleStarPattern.c

---
+++ ReverseRightTriangleStarPattern.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main()
+{
+    int i, j,n;
+    printf("Enter number of rows: ");
+    scanf("%d", &n);
+    for(i=0; i<n; i++)
+    {
+        for(j=i; j<n; j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
+
+
+OUTPUT-
+Enter number of rows: 5                                                                                                                                                      
+*****                                                                                                                                                                        
+****                                                                                                                                                                         
+***                                                                                                                                                                          
+**                                                                                                                                                                           
+* 
